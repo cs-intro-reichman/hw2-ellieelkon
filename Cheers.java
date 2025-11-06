cf86f851b3164601e8d6d401e7e4470aceb3451f
Ellie Elkon <ellieelkon@Ellies-MacBook-Pro.local>
2025-11-06 20:11:52 +0200

    done till 4

--- Cheers.java
+++ Cheers.java
@@ -9,21 +9,22 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
                 String word = args[0] ;
+                String upper = word.toUpperCase() ;
                 int repetition = Integer.parseInt(args[1]) ;
 
-                for (int i = 0; i < word.length(); i++) {
-                        if (word.charAt(i) == 'A' || word.charAt(i) == 'E' || word.charAt(i) == 'F' || word.charAt(i) == 'H' 
-                                || word.charAt(i) == 'I' || word.charAt(i) == 'L' || word.charAt(i) == 'M' ||word.charAt(i) == 'N' 
-                                ||word.charAt(i) == 'O' ||word.charAt(i) == 'R' || word.charAt(i) == 'S' || word.charAt(i) == 'X') {
-                                System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!") ;
+                for (int i = 0; i < upper.length(); i++) {
+                        if (upper.charAt(i) == 'A' || upper.charAt(i) == 'E' || upper.charAt(i) == 'F' || upper.charAt(i) == 'H' 
+                                || upper.charAt(i) == 'I' || upper.charAt(i) == 'L' || upper.charAt(i) == 'M' ||upper.charAt(i) == 'N' 
+                                ||upper.charAt(i) == 'O' ||upper.charAt(i) == 'R' || upper.charAt(i) == 'S' || upper.charAt(i) == 'X') {
+                                System.out.println("Give me an " + upper.charAt(i) + ": " + upper.charAt(i) + "!") ;
                         } else {
-                                System.out.println("Give me a  " + word.charAt(i) + ": " + word.charAt(i) + "!") ; }
+                                System.out.println("Give me a  " + upper.charAt(i) + ": " + upper.charAt(i) + "!") ; }
                         }
 
                 System.out.println("What does that spell?");
                 
                 for (int j = 0; j < repetition; j++) {
-                        System.out.println(word + "!!!");
+                        System.out.println(upper + "!!!");
                 }        
         }
 }
