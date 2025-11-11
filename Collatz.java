54c034ac5bb5e8fb4a4cd44cbb19bce04664dcd1
Ellie Elkon <ellieelkon@Ellies-MacBook-Pro.local>
2025-11-11 18:36:14 +0200

    done

--- Collatz.java
+++ Collatz.java
@@ -2,25 +2,27 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    int seed = Integer.parseInt(args[0]);
-		//String mode = args[1];
-		int count = 0;
-		int mode = Integer.parseInt(args[1]); 
-		int N = 0	
+		String mode = args[1];
 		
-		do { 
-			if (N % 2 == 0) {
-				N /= 2;
-			} else {
-				N = N * 3 + 1;
+		if (mode.equals("v")) {
+			for (int i = 1; i <= seed; i++){
+				int num = i;
+				int count = 1;
+				System.out.print(num + " ");
+				do { 
+				if (num % 2 == 0) {
+					num /= 2;
+				} else {
+					num = num * 3 + 1;
+				}
+				System.out.print(num + " ");
+				count++;	
+				} while (num != 1);
+					System.out.println("(" + count + ")");
 			}
-			count++;
-		} while (N < 1 && N >= seed);
-
-		System.out.println(count);
-
-		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
-
-
-
+					System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+			} else if (mode.equals("c")) {
+    		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}	 
 	}
 }
