45ae5d5f82b226e0a196b0d5a787553be9af7e80
Ellie Elkon <ellieelkon@Ellies-MacBook-Pro.local>
2025-11-12 12:58:21 +0200

    done

--- CalcPi.java
+++ CalcPi.java
@@ -3,12 +3,11 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int num = Integer.parseInt(args[0]) ;
-		int counted = 1 ;
 		double total = 0;
 		int i = 1;
 
 		while (i <= num) {
-			double a = 1 / num;
+			double a = 1.0 / (2 * i - 1);
 			
 			if (i % 2 == 0){
 				total = total - a;
@@ -16,7 +15,6 @@ public class CalcPi {
 				total = total + a;
 			}
 			i++;
-			counted = counted + 2;
 		}
 
 		System.out.println("pi according to Java: " + Math.PI) ; 
